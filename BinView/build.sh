d94b0a53e158bf62cfce3ab9b1b2ebcec46080f2
mkaraki <26180919+mkaraki@users.noreply.github.com>
2025-03-26 16:14:32 +0900

    Add dotnet install retry

--- BinView/build.sh
+++ BinView/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -fsSL https://dot.net/v1/dotnet-install.sh -o dotnet-install.sh
+curl -fsSL --retry 5 https://dot.net/v1/dotnet-install.sh -o dotnet-install.sh
 chmod +x dotnet-install.sh
 ./dotnet-install.sh -c 8.0 -InstallDir ./dotnet
 ./dotnet/dotnet publish -c Release -o output
